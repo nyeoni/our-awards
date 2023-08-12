0cc57996de156ab97d29deb81e2a99e36db50883
nyeoni <skdusdl8804@gmail.com>
2023-08-13 04:44:10 +0900

    :bug: fix: Fix state rendering error

--- src/context/ProtectedRoute.tsx
+++ src/context/ProtectedRoute.tsx
@@ -1,19 +1,28 @@
 'use client';
 
+import { stat } from 'fs';
 import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 import { redirect } from 'next/navigation';
+
+import { Spinner } from '@nextui-org/react';
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
 
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      redirect('/api/auth/signin');
+    }
+  }, [status]);
+
   if (status === 'loading') {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex justify-center w-full h-full">
+        <Spinner color="default" />
+      </div>
+    );
   }
-
-  if (status === 'unauthenticated') {
-    redirect('/api/auth/signin');
-  }
-
   return <>{children}</>;
 }
