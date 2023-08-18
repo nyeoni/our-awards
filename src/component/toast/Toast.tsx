e6dee7b126300055d4d52c4313682b9e8d77d2ce
nyeoni <skdusdl8804@gmail.com>
2023-08-18 22:55:48 +0900

    :sparkles: feat: Update Toast UI with animation

--- src/component/toast/Toast.tsx
+++ src/component/toast/Toast.tsx
@@ -1,27 +1,41 @@
+import { AnimatePresence, motion, useAnimation } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
+import { Portal } from '../portal';
+import styles from './Toast.module.css';
+
 interface ToastProps {
+  isVisible: boolean;
   message: string;
   duration?: number;
 }
 
-function Toast({ message, duration = 3000 }: ToastProps) {
-  const [visible, setVisible] = useState(true);
+function Toast({ isVisible, message, duration = 3000 }: ToastProps) {
+  const [visible, setVisible] = useState(isVisible);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    setTimeout(() => {
       setVisible(false);
     }, duration);
-
-    return () => clearTimeout(timer);
-  }, [duration]);
-
-  if (!visible) return null;
+  }, []);
 
   return (
-    <div className="fixed bottom-4 w-full rounded bg-primary/[0.06] shadow-md flex justify-center">
-      {message}
-    </div>
+    <AnimatePresence>
+      {visible && (
+        <Portal>
+          <div className={styles.container}>
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, transition: { ease: 'easeOut', duration: 0.2 } }}
+              className={styles.base}
+            >
+              {message}
+            </motion.div>
+          </div>
+        </Portal>
+      )}
+    </AnimatePresence>
   );
 }
 
