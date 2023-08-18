3e340274e3c39fbd371e112021ad2e245a4fe822
nyeoni <skdusdl8804@gmail.com>
2023-08-18 22:56:15 +0900

    :sparkles: feat: Update Portal component

---
+++ src/component/portal/Portal.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface PortalProps {
+  isOverlay?: boolean;
+  children: ReactNode;
+}
+
+export const Portal = ({ children, isOverlay = false }: PortalProps) => {
+  const ref = useRef<Element | null>(null);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.querySelector<HTMLElement>('#portal');
+    setMounted(true);
+  }, []);
+
+  return mounted && ref.current ? createPortal(<>{children}</>, ref.current) : null;
+};
