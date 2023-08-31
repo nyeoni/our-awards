1e06b8c202f7db2128501834d89bb390dedb1207
nyeoni <skdusdl8804@gmail.com>
2023-08-31 21:29:14 +0900

    :hammer: refactor: NavigateButton Props 수정 및 업데이트

--- src/app/award/components/buttons/NavigateButton.tsx
+++ src/app/award/components/buttons/NavigateButton.tsx
@@ -1,20 +1,21 @@
 import { ComponentProps } from 'react';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import BackIcon from '/public/svgs/back.svg';
 
-interface Props extends ComponentProps<'a'> {
-  path: string;
-}
-
-export function NavigateButton({ className = '', path }: Props) {
+export function NavigateButton({
+  label,
+  className = '',
+  ...props
+}: { label: string } & ComponentProps<'button'>) {
   const classes = `flex items-center text-sm w-fit py-2 ${className}`;
 
   return (
-    <Link className={classes} href={path}>
+    <button className={classes} {...props}>
       <BackIcon style={{ color: 'white' }} />
-      <span className="ml-0.5 text-xs font-medium">메인페이지로</span>
-    </Link>
+      <span className="ml-0.5 text-xs font-medium">{label}</span>
+    </button>
   );
 }
