6a8de4f0acdb3556b8761cfc60bf25caf681b74d
nyeoni <skdusdl8804@gmail.com>
2023-09-05 18:47:08 +0900

    :green_heart: chore: sentry authToken 추가

--- next.config.js
+++ next.config.js
@@ -43,6 +43,7 @@ module.exports = withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
+    authToken: process.env.SENTRY_AUTH_TOKEN,
     // Suppresses source map uploading logs during build
     silent: true,
 
