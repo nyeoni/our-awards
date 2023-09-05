05c95c34ec7798f4ff2f80cf515d1d2356967c07
nyeoni <skdusdl8804@gmail.com>
2023-09-05 18:27:08 +0900

    :green_heart: chore: Fix sentry lint err

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from '@sentry/nextjs';
 
-
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
@@ -21,6 +20,7 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
+    // eslint-disable-next-line import/namespace
     new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -4,7 +4,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from '@sentry/nextjs';
 
-
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
